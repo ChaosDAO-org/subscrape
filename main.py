250c64e3839ca7e393393c7864e6a9b26bcd9cf4
Tommi Enenkel <tommi.enenkel@gmail.com>
2022-02-15 15:20:28 +0100

    use api key

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import json
 import requests
 from datetime import datetime
 import logging
+import os
+
+# Setup
+## Logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger()
-
 log.debug("Hello World!")
 
-endpoint = "https://kusama.api.subscan.io"
+## Load Subscan API Key
+api_key = None
+if os.path.exists("subscan-key"):
+    f = open("subscan-key")
+    api_key = f.read()
+log.debug(api_key)
 
-def query(method, headers = {}, body = {}):
-  url = endpoint + method
+# Methods
+
+def query(url, headers = {}, body = {}):
   headers["Content-Type"] = "application/json"
+  headers["x-api-key"] = api_key
   body = json.dumps(body)
   before = datetime.now()
   response = requests.post(url, headers = headers, data = body)
@@ -20,11 +30,13 @@ def query(method, headers = {}, body = {}):
   log.info(response.headers)
   return response.text
 
+# Main
 
-api_key = None
-request_rate = 0.5 # a call every 2 seconds
+endpoint = "https://kusama.api.subscan.io"
+method = "/api/scan/transfers"
+url = endpoint + method
 
 for i in range(1):
-    response = query("/api/scan/transfers", body= {"row": 1, "page": 1})
+    response = query(url, body= {"row": 1, "page": 1})
     log.info(response)
     obj = json.loads(response)
