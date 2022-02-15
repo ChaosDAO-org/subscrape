be9d8842f9408bab5d6ada12de8bae181801d0a8
Tommi Enenkel <tommi.enenkel@gmail.com>
2022-02-15 15:55:20 +0100

    add timeout to make requests faster

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from datetime import datetime
 import logging
 import os
 
+import http.client
+http.client.HTTPConnection.debuglevel = 1
+
 # Setup
 ## Logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger()
 log.debug("Hello World!")
+
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 ## Load Subscan API Key
 api_key = None
@@ -24,7 +31,12 @@ def query(url, headers = {}, body = {}):
   headers["x-api-key"] = api_key
   body = json.dumps(body)
   before = datetime.now()
-  response = requests.post(url, headers = headers, data = body)
+  # TE: there seems to be an issue with the way the requests library handles the server response
+  # the request will only conclude successfully after it timed out. one possible reason could be
+  # that the server is sending no content-length header. I tried adding the timeout param and it
+  # forces a faster timeout and successful conclusion of the request.
+  # Possibly related discussion: https://github.com/psf/requests/issues/4023
+  response = requests.post(url, headers = headers, data = body, timeout=1)
   after = datetime.now()
   log.debug("request took: " + str(after - before))
   log.info(response.headers)
