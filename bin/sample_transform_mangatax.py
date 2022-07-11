b4350bb35acf06f6f6ae304d75c383c8ea601343
Tommi Enenkel <tommi.enenkel@gmail.com>
2022-07-11 08:32:28 +0200

    #20 prepare PyPI - create pyproject.toml

--- bin/sample_transform_mangatax.py
+++ bin/sample_transform_mangatax.py
@@ -1,5 +1,7 @@
 import csv
 import json
+
+import pandas as pd
 from subscrape.db.subscrape_db import SubscrapeDB
 from substrateinterface.utils import ss58
 import logging
@@ -9,7 +11,17 @@ import datetime
 config = {
     "mangatax":{
         "events":{
-            "xyk": ["assetsswapped"]
+            "xyk": [
+                "assetsswapped", 
+            #    "rewardsclaimed", 
+            #    "liquidityminted", 
+            #    "liquidityactivated", 
+            #    "liquiditydeactivated", 
+            #    "liquidityburned"
+            ],
+            #"bootstrap": ["provisioned", "rewardsclaimed"],
+            #"xtokens": ["transferred"],
+            #"parachain_staking": ["rewarded"]
         }
     }
 }
@@ -33,16 +45,6 @@ def fetch_swaps():
         params = json.loads(event["params"])
         params = unwrap_event_params(params)
 
-        if int(params[1]["TokenId"]) == 0:
-            params[2]["Balance"] = int(params[2]["Balance"]) / 1e18
-        else:
-            params[2]["Balance"] = int(params[2]["Balance"]) / 1e12
-
-        if int(params[3]["TokenId"]) == 0:
-            params[4]["Balance"] = int(params[4]["Balance"]) / 1e18
-        else:
-            params[4]["Balance"] = int(params[4]["Balance"]) / 1e12
-
         swaps.append([
             event["event_index"],
             event["block_timestamp"],
@@ -53,6 +55,24 @@ def fetch_swaps():
             params[3]["TokenId"],
             params[4]["Balance"]
         ])
+
+    # create a dataframe from the swaps
+    df = pd.DataFrame(swaps, columns=["event_index", "block_timestamp", "datetime", "address", "sold_asset_id", "sold_asset_amount", "bought_asset_id", "bought_asset_amount"])
+    # filter df to only include sold_asset_id == 0
+    df_sold_mgx = df[df["sold_asset_id"] == 0]
+    # add a column `ratio` that is the bought_asset_amount / sold_asset_amount
+    df_sold_mgx["ratio"] = df_sold_mgx["bought_asset_amount"] / df_sold_mgx["sold_asset_amount"]
+    # filter df to only include bought_asset_id == 0
+    df_bought_mgx = df[df["bought_asset_id"] == 0]
+    # add a column `ratio` that is the sold_asset_amount / bought_asset_amount
+    df_bought_mgx["ratio"] = df_bought_mgx["sold_asset_amount"] / df_bought_mgx["bought_asset_amount"]
+    # merge the two dataframes
+    df_merged = pd.merge(df_sold_mgx, df_bought_mgx, on="event_index")
+    # add a column `date` thate is the date of the event
+    df_merged["date"] = df_merged["datetime"].dt.date
+    # add a column `hour` that is the hour of the event
+
+    pass
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
